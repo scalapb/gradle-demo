1bc60bcb585d67779300cbfbe984615d476dab50
Nadav Samet <thesamet@gmail.com>
2019-05-19 11:49:51 -0700

    move test suite

--- src/test/scala/scalapb/demo/ProtoSuite.scala
+++ src/test/scala/scalapb/demo/ProtoSuite.scala
@@ -1,4 +1,4 @@
-package scalapb.gradle
+package scalapb.demo
 
 import org.scalatest.FunSuite
 import org.junit.runner.RunWith
@@ -7,7 +7,7 @@ import org.scalatest.junit.JUnitRunner
 import scalapb.protos.demo.Person
 
 @RunWith(classOf[JUnitRunner])
-class LibrarySuite extends FunSuite {
+class ProtoSuite extends FunSuite {
   def person = Person(name="John", age=38)
 
   test("toByteArray is the inverse of parseFrom") {
